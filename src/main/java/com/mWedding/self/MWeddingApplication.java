a7b7fb352f017ac3bce3a151618cef722b58a884
PARKHEEJAE <daecadid@naver.com>
2020-06-06 11:10:17 +0900

    test commit

---
+++ src/main/java/com/mWedding/self/MWeddingApplication.java
@@ -0,0 +1,13 @@
+package com.mWedding.self;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MWeddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MWeddingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mWedding/self/MWeddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mWedding.self;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MWeddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
